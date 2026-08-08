dffa96b9f50862d35722e4589133330d7946c245
nansari7287-sys <nansari7287@gmail.com>
2026-08-09 00:04:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'kommodo.ai' },
+      { protocol: 'https', hostname: 'i.ibb.co' },
+      { protocol: 'https', hostname: 'cdn.phototourl.com' },
+      { protocol: 'https', hostname: 'github.com' },
+      { protocol: 'https', hostname: 'raw.githubusercontent.com' }
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
